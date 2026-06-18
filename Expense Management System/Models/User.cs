b0d508a14ac00e53f410b5c4ecaef00919a18ff4
Yuvan Raj <evanraj2000@gmail.com>
2026-06-18 17:43:31 +0530

    Added User Model

---
+++ Expense Management System/Models/User.cs
@@ -0,0 +1,19 @@
+﻿namespace Expense_Management_System.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string PasswordHash { get; set; } = string.Empty;
+
+        public int RoleId { get; set; }
+
+        public int DepartmentId { get; set; }
+
+        public int? ManagerId { get; set; }
+    }
+}
