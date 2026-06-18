8efd196d78bcebdc46f61cfa630843b34985b93c
Yuvan Raj <evanraj2000@gmail.com>
2026-06-18 18:24:16 +0530

    "Added Department model and DbContext update"

---
+++ Expense Management System/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Expense_Management_System.Models;
+
+namespace Expense_Management_System.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Department> Departments { get; set; }
+    }
+}
